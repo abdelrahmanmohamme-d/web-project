e750b33991e234575af73e3733daf6f42536b44a
abdelrahmanmohamme-d <abdelrahman.m7mmd00@gmail.com>
2024-12-12 22:25:25 +0200

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // Get all buttons and sections
   const buttons = {
     homeBtn: document.getElementById('homeBtn'),
     aboutBtn: document.getElementById('aboutBtn'),
@@ -16,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     ww2: document.getElementById('ww2')
   };
 
-  // Show selected section and hide others
+ 
   const showSection = (sectionId) => {
     for (const key in sections) {
       sections[key].classList.add('hidden');
@@ -24,13 +23,13 @@ document.addEventListener('DOMContentLoaded', () => {
     sections[sectionId].classList.remove('hidden');
   };
 
-  // Event listeners for navigation buttons
+  
   buttons.homeBtn.addEventListener('click', () => showSection('home'));
   buttons.aboutBtn.addEventListener('click', () => showSection('about'));
   buttons.contactBtn.addEventListener('click', () => showSection('contact'));
   buttons.ww1Btn.addEventListener('click', () => showSection('ww1'));
   buttons.ww2Btn.addEventListener('click', () => showSection('ww2'));
 
-  // Show the Home section by default
+  
   showSection('home');
 });
